da92e167619020dbd05bf8fa3dca146d1bd003de
git-illite <71469298+git-illite@users.noreply.github.com>
2023-07-27 22:21:50 -0400

    created navMenu component and css

---
+++ app/NavMenu.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function NavMenu(){
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
