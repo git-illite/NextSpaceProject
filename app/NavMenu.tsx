6a7c5d9ed39e8f6310bafe287895128a372d129a
git-illite <71469298+git-illite@users.noreply.github.com>
2023-07-27 22:44:13 -0400

    added logo and nav menu

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -1,11 +1,23 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from './page.module.css'
-
+import styles from './NavMenu.module.css'
 export default function NavMenu(){
     return (
-        <nav>
-            
+        <nav className={styles.nav}>
+            <Link href={'/'}>
+            <Image src='/logo.png' width={70} height={60} alt='NextSpace Logo'/>ext Space          
+            </Link>
+    <ul className={styles.links}>
+      <li>
+        <Link href={"/about"}>About</Link>
+      </li>
+      <li>
+        <Link href={"/blog"}> Blog</Link>
+      </li>
+      <li>
+        <Link href={"/users"}>Users</Link>
+      </li>
+    </ul>
         </nav>
     )
 }
